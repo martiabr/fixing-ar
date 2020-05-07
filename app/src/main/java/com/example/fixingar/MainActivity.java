bf5d4d2b17f22008fe0f41cc169d88f2c6b263c2
oKeding <oskar.keding@gmail.com>
2020-05-07 18:10:22 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/fixingar/MainActivity.java
+++ app/src/main/java/com/example/fixingar/MainActivity.java
@@ -323,7 +323,7 @@ public class MainActivity extends CameraActivity implements CvCameraViewListener
             if (detectedMarkers.size() >= 5) {
                 Log.d("howmany","Detected 5 markers.");
                 Log.d("markerPoints", String.valueOf(detectedMarkers.toArray().toString()));
-                Mat dst = perspectiveFixer.fixPerspectiveMultipleMarker(mRgba,detectedMarkers,MARKER_SIZE);
+                Mat dst = perspectiveFixer.fixPerspectiveMultipleMarker(mRgba,detectedMarkers,MARKER_SIZE,mCoordinates);
                 return mRgba;
             }
           
