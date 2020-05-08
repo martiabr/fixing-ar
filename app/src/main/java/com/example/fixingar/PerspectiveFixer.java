61a817b5e4890eb0a5c7b1bf008611ceb79f81b2
oKeding <oskar.keding@gmail.com>
2020-05-08 09:10:21 +0200

    Update PerspectiveFixer.java

--- app/src/main/java/com/example/fixingar/PerspectiveFixer.java
+++ app/src/main/java/com/example/fixingar/PerspectiveFixer.java
@@ -225,6 +225,7 @@ public class PerspectiveFixer {
             // Following used for debugging, instead of
             Point[] coloredP = cornersDeviceTr.toArray();
             Log.d("ColoredP", coloredP[0].toString() + coloredP[1].toString() + coloredP[2].toString() + coloredP[3].toString());
+
             for (int i = 0; i < 4; i++) {
                 drawLine(rgba, coloredP[i % 4], coloredP[(i + 1) % 4]);
             }
