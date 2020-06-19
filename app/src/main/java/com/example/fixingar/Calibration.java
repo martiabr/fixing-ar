c5a6b2afc0563bfbed12598366b4f1023319f017
blueberryzosa <61871292+blueberryzosa@users.noreply.github.com>
2020-06-19 13:50:09 +0200

    switching in between screens

--- app/src/main/java/com/example/fixingar/Calibration.java
+++ app/src/main/java/com/example/fixingar/Calibration.java
@@ -1,13 +1,30 @@
 package com.example.fixingar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Calibration extends AppCompatActivity {
+    private Button mReturnButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calibration);
+
+        mReturnButton = findViewById(R.id.button_return2);
+        mReturnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ReturnToSettings();
+            }
+        });
+    }
+
+    public void ReturnToSettings() {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/fixingar/Settings.java
+++ app/src/main/java/com/example/fixingar/Settings.java
@@ -1,13 +1,43 @@
 package com.example.fixingar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
+    private Button mCalibrateButton;
+    private Button mReturnButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        mCalibrateButton = findViewById(R.id.button_calibrate);
+        mCalibrateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoToCalibration();
+            }
+        });
+        mReturnButton = findViewById(R.id.button_return);
+        mReturnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ReturnToMain();
+            }
+        });
+    }
+
+    public void GoToCalibration() {
+        Intent intent = new Intent(this, Calibration.class);
+        startActivity(intent);
+    }
+
+    public void ReturnToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
