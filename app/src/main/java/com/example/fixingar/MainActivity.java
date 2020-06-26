0729597c2be8b0989dbf51d7d62eec0d980baa95
blueberryzosa <61871292+blueberryzosa@users.noreply.github.com>
2020-06-26 11:15:11 +0200

    cleaned up face detection menu

--- app/src/main/java/com/example/fixingar/MainActivity.java
+++ app/src/main/java/com/example/fixingar/MainActivity.java
@@ -80,12 +80,6 @@ public class MainActivity extends CameraActivity implements CvCameraViewListener
     public static final int        JAVA_DETECTOR       = 0;
     public static final int        NATIVE_DETECTOR     = 1;
 
-    private MenuItem               mItemFace50;
-    private MenuItem               mItemFace40;
-    private MenuItem               mItemFace30;
-    private MenuItem               mItemFace20;
-    private MenuItem               mItemType;
-
     private Mat                    mRgba;
     private Mat                    mGray;
     private File                   mCascadeFile1;
@@ -98,12 +92,7 @@ public class MainActivity extends CameraActivity implements CvCameraViewListener
     private int                    mDetectorType       = JAVA_DETECTOR;
     private String[]               mDetectorName;
 
-    private float                  mRelativeEyeSize   = 0.1f; // change this parameter to adjust min Eye size
-    private int                    mAbsoluteEyeSize   = 0;
     private float[]                mCoordinates = new float[4]; //x and y position in m
-
-    private float                  mRelativeFaceSize   = 0.2f; // change this parameter to adjust min Face size
-    private int                    mAbsoluteFaceSize   = 0;
     private FaceDetection          faceDetection;
 
     private CameraBridgeViewBase mOpenCvCameraView;
@@ -359,17 +348,6 @@ public class MainActivity extends CameraActivity implements CvCameraViewListener
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        Log.i(TAG, "called onCreateOptionsMenu");
-        mItemFace50 = menu.add("Face size 50%");
-        mItemFace40 = menu.add("Face size 40%");
-        mItemFace30 = menu.add("Face size 30%");
-        mItemFace20 = menu.add("Face size 20%");
-        mItemType   = menu.add(mDetectorName[mDetectorType]);
-        return true;
-    }
-
     private Runnable mCameraSwitchRunnable = new Runnable() {
         @Override
         public void run() {
@@ -411,33 +389,6 @@ public class MainActivity extends CameraActivity implements CvCameraViewListener
         }
 
         timerRunning = !timerRunning;
-}
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Log.i(TAG, "called onOptionsItemSelected; selected item: " + item);
-        if (item == mItemFace50)
-            setMinFaceSize(0.5f);
-        else if (item == mItemFace40)
-            setMinFaceSize(0.4f);
-        else if (item == mItemFace30)
-            setMinFaceSize(0.3f);
-        else if (item == mItemFace20)
-            setMinFaceSize(0.2f);
-        else if (item == mItemType) {
-            int tmpDetectorType = (mDetectorType + 1) % mDetectorName.length;
-            item.setTitle(mDetectorName[tmpDetectorType]);
-            setDetectorType(tmpDetectorType);
-        }
-        return true;
-    }
-
-    private void setMinFaceSize(float faceSize) {
-        mRelativeFaceSize = faceSize;
-        mAbsoluteFaceSize = 0;
-    }
-    private void setMinEyeSize(float eyeSize) {
-        mRelativeEyeSize = eyeSize;
-        mAbsoluteEyeSize = 0;
     }
 
     private void setDetectorType(int type) {
